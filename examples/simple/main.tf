a46b9a6b9ecc5023974730197d8295f6e678a153
Rachid <ouattchidi@gmail.com>
2019-09-12 21:19:23 +0200

    minor

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,18 +1,19 @@
-resource "openstack_compute_keypair_v2" "keypair" {
-  name = "my-keypair"
-}
+# resource "openstack_compute_keypair_v2" "keypair" {
+#   name = "my-keypair"
+# }
 
 module "jenkins" {
   source = "../../"
 
   enable_jenkins_master              = "1"
-  jenkins_master_name                = "jenkins-test"
+  jenkins_master_name                = "dnv-jenkins"
   jenkins_master_instance_count      = 1
   jenkins_master_image_name          = "Jenkins Master"
   jenkins_master_compute_flavor_name = "dinivas.large"
-  jenkins_master_keypair_name        = "${openstack_compute_keypair_v2.keypair.name}"
-  jenkins_master_network             = "dnv-mgmt"
-  jenkins_master_subnet              = "dnv-mgmt-subnet"
-  jenkins_master_floating_ip_pool    = ""
+  #jenkins_master_keypair_name        = "${openstack_compute_keypair_v2.keypair.name}"
+  jenkins_master_keypair_name     = "dnv"
+  jenkins_master_network          = "dnv-mgmt"
+  jenkins_master_subnet           = "dnv-mgmt-subnet"
+  jenkins_master_floating_ip_pool = "public"
 }
 
--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "jenkins_master_instance_ids" {
 }
 
 output "jenkins_master_floating_ip" {
-  value       = "${openstack_networking_floatingip_v2.jenkins_master_floatingip}"
+  value       = "${openstack_networking_floatingip_v2.jenkins_master_floatingip.0.address}"
   description = "The floating ips bind to Jenkins master"
 }
